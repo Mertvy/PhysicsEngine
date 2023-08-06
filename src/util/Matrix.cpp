966c276a804c5093a27770ce0003bf015c6a5cd8
Nicholas Weiss <njweiss4@gmail.com>
2023-08-07 00:16:12 +0200

    start line collision

---
+++ src/util/Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicholas Weiss on 7/31/23.
+//
+
+#include "Matrix.h"
---
+++ src/util/Matrix.h
@@ -0,0 +1,16 @@
+//
+// Created by Nicholas Weiss on 7/31/23.
+//
+
+#ifndef PHYSICS_ENGINE_MATRIX_H
+#define PHYSICS_ENGINE_MATRIX_H
+
+
+
+class Matrix {
+
+};
+
+
+
+#endif //PHYSICS_ENGINE_MATRIX_H
